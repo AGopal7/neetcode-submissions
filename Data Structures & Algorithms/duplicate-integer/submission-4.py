2319d94c4be5d5a8e3fa304bf80c79926c2a5f26
AGopal7 <155612764+AGopal7@users.noreply.github.com>
2026-06-15 16:07:13 -0700

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        Saved_value = {}
+        for i in range (len(nums)):
+
+            if nums[i] in Saved_value:
+
+                #print(f" the number {nums[i]} already exists in the list at position{i}"  )
+                return True
+                
+            else:
+                Saved_value[nums[i]] = True
+        return False
